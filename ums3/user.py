13b3eb34dddf535d8fe485914361edcf6887e73f
Yasunori Ishiguro <xjr1300.04@gmail.com>
2024-05-21 15:14:26 +0900

    Fix the explanation of outputing logs.

--- ums3/user.py
+++ ums3/user.py
@@ -1,6 +1,10 @@
+import json
+import uuid
 from dataclasses import dataclass
-from enum import Enum
+from datetime import datetime
+from enum import Enum, StrEnum, auto
 from typing import Any, Dict, List
+from zoneinfo import ZoneInfo
 
 from .company import Company
 
@@ -14,28 +18,64 @@ class UserType(Enum):
     Employee = 1
 
 
-class UserEventType(Enum):
+class UserEventType(StrEnum):
     """ユーザーに発生したイベントの種類"""
 
     # ユーザーが登録された
-    Registered = 1
+    Registered = auto()
     # ユーザーのEメールアドレスが変更された
-    EmailChanged = 2
+    EmailChanged = auto()
     # ユーザーの種類が変更された
-    TypeChanged = 3
+    TypeChanged = auto()
     # ユーザーが削除された
-    Deleted = 4
+    Deleted = auto()
 
 
 @dataclass
 class UserEvent:
     """ユーザーに発生したイベント"""
 
+    # イベントID
+    event_id: uuid.UUID
+
+    # 発生日時
+    dt: datetime
+
     # ユーザーに発生したイベントの種類
     event_type: UserEventType
 
     # ユーザに発生したイベントのデータ
     event_data: Dict[str, Any]
+
+    def __init__(
+        self, event_id: uuid.UUID, event_type: UserEventType, event_data: Dict[str, Any]
+    ) -> None:
+        """イニシャライザ
+
+        Args:
+            event_id (uuid.UUID): イベントID
+            event_type (UserEventType): ユーザーイベントの種裏
+            event_data (Dict[str, Any]): イベントデータ
+        """
+        self.event_id = event_id
+        self.dt = datetime.now(tz=ZoneInfo("Asia/Tokyo"))
+        self.event_type = event_type
+        self.event_data = event_data
+
+    def to_json(self) -> str:
+        """ユーザーイベントを表現するJSON形式の文字列を返す。
+
+        Returns:
+            str: ユーザーイベントを表現するJSON形式の文字列
+        """
+        data = {
+            "event_id": self.event_id,
+            "dt": self.dt.isoformat(),
+            "domain": "user",
+            "event_type": self.event_type,
+        }
+        data.update(self.event_data)
+        return json.dumps(data, ensure_ascii=False)
 
 
 class User:
@@ -65,6 +105,7 @@ class User:
             new_email (str): 新しいEメールアドレス
             company (Company): 会社
         """
+        event_id = uuid.uuid4()
         if self.email == new_email:
             # 現在のEメールアドレスと新しいEメールアドレスが等しい場合、処理を終了
             return
@@ -83,9 +124,10 @@ class User:
             # ユーザーの種類が変更されたイベントを登録
             self.events.append(
                 UserEvent(
+                    event_id,
                     UserEventType.TypeChanged,
                     {
-                        "id": self.id,
+                        "user_id": self.id,
                         "before": self.type.name.lower(),
                         "after": new_user_type.name.lower(),
                     },
@@ -95,8 +137,13 @@ class User:
         # Eメール変更イベントを登録
         self.events.append(
             UserEvent(
+                event_id,
                 UserEventType.EmailChanged,
-                {"id": self.id, "before": self.email, "after": new_email},
+                {
+                    "user_id": self.id,
+                    "before": self.email,
+                    "after": new_email,
+                },
             )
         )
 
