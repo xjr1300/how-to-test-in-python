08a9c06339bf5cfb62cc2305d423350b1933bcdf
kuroyasu <xjr1300.04@gmail.com>
2024-05-29 10:20:03 +0900

    Fix the definition of the sale detail.

--- drugstore/domain/models/sales.py
+++ drugstore/domain/models/sales.py
@@ -4,14 +4,16 @@ from datetime import datetime
 from decimal import Decimal
 from typing import List, Optional
 
-from .customers import Customer
-from .items import Item
+from drugstore.domain.models.customers import Customer
+from drugstore.domain.models.items import Item
 
 
 @dataclass
 class SaleDetail:
     """売上明細"""
 
+    # 売上ID
+    id: uuid.UUID
     # 商品
     item: Item
     # 数量
@@ -38,6 +40,8 @@ class Sale:
     discount_rate: Decimal
     # 割引額
     discount_amount: Decimal
+    # 課税対象額
+    taxable_amount: Decimal
     # 消費税率
     consumption_tax_rate: Decimal
     # 消費税額
